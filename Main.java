1631fa0cf45452cde08022ba51c44064899d2f72
RonakHegde17 <139830762+RonakHegde17@users.noreply.github.com>
2024-03-30 21:19:59 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,34 +3,47 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        boolean loggedIn = false;
+        String email;
+        String username = "";
+        String password = "";
+        int logChoice;
 
         System.out.println("Welcome to NotFacebook!");
-        System.out.println("1: Sign Up");
-        System.out.println("2: Login");
-
-        boolean loggedIn = false;
-        int logChoice = scanner.nextInt();
-        scanner.nextLine();
 
         while (!loggedIn) {
+            System.out.println("1: Sign Up");
+            System.out.println("2: Login");
+
+
+            logChoice = scanner.nextInt();
+            scanner.nextLine();
+
 
             switch (logChoice) {
                 case 1:
                     System.out.println("Enter your email:");
-                    String email = scanner.nextLine();
+                    email = scanner.nextLine();
+                    System.out.println("Enter a username:");
+                    username = scanner.nextLine();
                     System.out.println("Enter your password:");
-                    String password = scanner.nextLine();
+                    password = scanner.nextLine();
+
 
                     break;
-
 
                 case 2:
                     System.out.println("Enter your username:");
-                    System.out.println("Enter your password:");
+                    if (scanner.nextLine().equals(username)) {
+                        System.out.println("Enter your password");
+                        if (scanner.nextLine().equals(password)) {
+                            System.out.println("Login Successful!");
+                            loggedIn = true;
+
+                        }
+                    }
                     break;
             }
-
-
         }
     }
 }
