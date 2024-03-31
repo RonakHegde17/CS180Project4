5144d4463ab7916a51359eacc096ff268083ca8e
kkita8 <165527193+kkita8@users.noreply.github.com>
2024-03-31 13:35:01 -0500

    Update DatabaseInterface.java

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -3,5 +3,6 @@ import java.util.ArrayList;
 public interface DatabaseInterface {
     public boolean readFile();
     public ArrayList<UserProfile> searchUsers(String userSearch);
-
+    public String viewUser(UserProfile user);
+    
 }
