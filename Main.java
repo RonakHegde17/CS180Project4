bb10c4512ec6483698c4816abde26c80011f0956
RonakHegde17 <139830762+RonakHegde17@users.noreply.github.com>
2024-03-30 20:59:12 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,29 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("im here");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to NotFacebook!");
+        System.out.println("1: Sign Up");
+        System.out.println("2: Login");
+
+        boolean loggingIn = false;
+        boolean signingUp = false;
+        int logChoice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (logChoice) {
+            case 1 -> System.out.println("Enter your");
+            case 2 -> System.out.println("bye");
+
+        }
+
+
+
+
+
+
+
     }
 }
