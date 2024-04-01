618484a69d4becf7fa7eba70167ab0ea22fff8c7
kkita8 <165527193+kkita8@users.noreply.github.com>
2024-03-31 22:35:15 -0400

    Update ServerInterface.java

--- ServerInterface.java
+++ ServerInterface.java
@@ -1,3 +1,13 @@
+/**
+ * NotFacebook ServerInterface
+ *
+ * brief description of the program
+ *
+ * @author your name, section number
+ *
+ * @version date of completion
+ *
+ */
 public interface ServerInterface {
     public void startServer();
     public void deleteMessage(int messageID, String sender, String recipient);
