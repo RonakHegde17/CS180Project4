856712fa713ac7f6423d1fd072d5e0cd1ae56596
RonakHegde17 <139830762+RonakHegde17@users.noreply.github.com>
2024-04-01 18:17:21 -0400

    Update UserProfileInterface.java

--- UserProfileInterface.java
+++ UserProfileInterface.java
@@ -15,8 +15,6 @@ import java.util.ArrayList;
 public interface UserProfileInterface {
     public String getUsername();
     public void setUsername(String username);
-    public String getEmail();
-    public void setEmail(String email);
     public String getPassword();
     public void setPassword(String password);
     public ArrayList<UserProfile> getFriends();
