234e54a4fbadeae34cfb0601f3f41029db597638
RonakHegde17 <139830762+RonakHegde17@users.noreply.github.com>
2024-04-01 17:11:01 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLOutput;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,41 +14,94 @@ public class Main {
         String username = "";
         String password = "";
         int logChoice;
+        boolean validUsername;
+        boolean validPassword;
+        int mainMenuChoice;
+
+        Database database = new Database("database.txt");
+        database.createFile();
+
+
 
         System.out.println("Welcome to NotFacebook!");
 
         while (!loggedIn) {
+            validUsername = false;
+            validPassword = false;
             System.out.println("1: Sign Up");
             System.out.println("2: Login");
-
-
             logChoice = scanner.nextInt();
             scanner.nextLine();
 
-
             switch (logChoice) {
                 case 1:
-                    System.out.println("Enter your email:");
-                    email = scanner.nextLine();
-                    System.out.println("Enter a username:");
-                    username = scanner.nextLine();
-                    System.out.println("Enter your password:");
-                    password = scanner.nextLine();
+                    while (!validUsername) {
+                        System.out.println("Enter a username:");
+                        username = scanner.nextLine();
+                        if (database.readFile1(username)) {
+                            System.out.println("Enter a password:");
+                            password = scanner.nextLine();
+                            System.out.println("Successfully Signed Up!");
+                            validUsername = true;
+                            database.writeFile1(username, password);
+                            UserProfile user = new UserProfile(username, password);
+                            database.addUser(user);
 
+
+                        }
+                    }
 
                     break;
 
                 case 2:
-                    System.out.println("Enter your username:");
-                    if (scanner.nextLine().equals(username)) {
-                        System.out.println("Enter your password");
-                        if (scanner.nextLine().equals(password)) {
+                    while (!validUsername) {
+                        System.out.println("Enter your username:");
+                        username = scanner.nextLine();
+                        if (database.readFile2(username)) {
+                            validUsername = true;
+                        } else {
+                            System.out.println("User does not exist!");
+                        }
+                    }
+                    while (!validPassword) {
+                        System.out.println("Enter your password:");
+                        password = scanner.nextLine();
+                        if (database.readFile3(username, password)) {
+                            validPassword = true;
                             System.out.println("Login Successful!");
                             loggedIn = true;
-
+                        } else {
+                            System.out.println("Invalid Password!");
                         }
                     }
                     break;
+
+
+
+            }
+
+        }
+
+        while (loggedIn) {
+
+
+            System.out.println("1: View Account Details");
+            System.out.println("2: View Friends");
+            System.out.println("3: Logout");
+            mainMenuChoice = scanner.nextInt();
+            scanner.nextLine();
+            switch (mainMenuChoice) {
+                case 1:
+                    System.out.println("Username: " + username);
+                    System.out.println("Password: " + password);
+                    break;
+                case 2:
+                    System.out.println("No Friends Added");
+                    break;
+
+                case 3:
+                    System.out.println("Logging Out...");
+                    loggedIn = false;
             }
         }
     }
