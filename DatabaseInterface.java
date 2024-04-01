8e605faf571bb601b15f74a484fdc10c52a14259
kkita8 <165527193+kkita8@users.noreply.github.com>
2024-04-01 15:36:38 -0400

    Update DatabaseInterface.java

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 /**
  * NotFacebook DatabaseInterface
  *
- * brief description of the program
+ * This program is the interface
+ * for the database class.
  *
- * @author your name, section number
+ * @author Team 5, 19
  *
- * @version date of completion
+ * @version April 1, 2024
  *
  */
 
