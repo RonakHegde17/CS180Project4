8e09f72821695bfa885e843031caf28f9b892c02
RonakHegde17 <139830762+RonakHegde17@users.noreply.github.com>
2024-04-01 17:11:41 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
