f6be2d0fa39cc6d8ec72efaed27a0e5971fb657a
RonakHegde17 <139830762+RonakHegde17@users.noreply.github.com>
2024-04-01 19:51:23 -0400

    Update DatabaseTests.java

--- DatabaseTests.java
+++ DatabaseTests.java
@@ -1,1 +1,51 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DatabaseTests {
+    Database database = new Database("TestDatabase.txt");
+
+    @Test
+    public void writeFileTest() { //THIS TEST WILL CREATE A NEW USER WITH THE INPUTTED INFORMATION. PASSING MEANS DATABASE CLASS CAN SUCESSFULLY ADD USERS TO THE DATABASE
+        database.createFile();
+        String username = "testUsername";
+        String password = "testPassword";
+        boolean signUpResult = database.writeFile1(username, password);
+        assertTrue(signUpResult);
+    }
+
+    @Test
+    public void existingUsernameTest() { //THIS TEST WILL ATTEMPT TO DETECT IF THE INPUTTED USERNAME ALREADY EXISTS. PASSING MEANS DATABASE WILL PREVENT SIGNING UP WITH DUPLICATE NAME
+        database.createFile();
+        String username = "testUsername";
+        String password = "testPassword";
+        boolean signUpResult = database.readFile1(username);
+        assertFalse(signUpResult);
+    }
+
+    @Test
+    public void readFileTest() { //THIS TEST WILL VERIFY THAT THE INPUTTED LOGIN MATCHES THE USER INFO IN THE DATABASE. PASSING MEANS LOGIN INFORMATION WAS CORRECT
+        database.createFile();
+        String username = "testUsername";
+        String password = "testPassword";
+        boolean loginResult = database.readFile3(username, password);
+        assertTrue(loginResult);
+    }
+    @Test
+    public void incorrectPassTest() { //THIS TEST WILL ATTEMPT TO LOG IN WITH AN INCORRECT PASSWORD. PASSING MEANS THE DATABASE CLASS SUCCESSFULLY PREVENTS LOGIN
+        database.createFile();
+        String username = "testUsername";
+        String password = "yabadabadoo";
+        boolean loginResult = database.readFile3(username, password);
+        assertFalse(loginResult);
+    }
+
+    @Test
+    public void incorrectUsernameTest() { //THIS TEST WILL ATTEMPT TO LOG IN WITH A USERNAME THAT DOESN'T EXIST. PASSING MEANS THE DATABASE CLASS SUCCESSFULLY PREVENTS LOGIN
+        database.createFile();
+        String username = "yabadabadoo";
+        String password = "testPassword";
+        boolean loginResult = database.readFile2(username);
+        assertFalse(loginResult);
+    }
+}
 
