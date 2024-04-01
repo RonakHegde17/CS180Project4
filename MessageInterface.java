c173005e863eeae4bd19b7cf8960533ab01c8f33
kkita8 <165527193+kkita8@users.noreply.github.com>
2024-03-31 22:33:37 -0400

    Update MessageInterface.java

--- MessageInterface.java
+++ MessageInterface.java
@@ -1,3 +1,13 @@
+/**
+ * NotFacebook MessageInterface
+ *
+ * brief description of the program
+ *
+ * @author your name, section number
+ *
+ * @version date of completion
+ *
+ */
 public interface MessageInterface  {
     public void getMessage();
     public void setMessage(String message);
