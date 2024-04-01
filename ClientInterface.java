55043de9286ba7f37ca3b624c667de4d43bc2d5c
wonc288 <155019702+wonc288@users.noreply.github.com>
2024-03-31 22:29:16 -0400

    Update ClientInterface.java

--- ClientInterface.java
+++ ClientInterface.java
@@ -1,1 +1,8 @@
-
+public interface ClientInterface {
+    public void sendMessage(String recipient, String message);
+    public void sendMessage(String message);
+    public void requestDeleteMessage(int messageID, String sender, String recipient);
+    public void connectToServer();
+    public void logout();
+    public void login(String username, String password);
+}
