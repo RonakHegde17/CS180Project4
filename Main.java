a6f6fb5a0b8f8184f8bbe035504921efdfb209f9
kkita8 <165527193+kkita8@users.noreply.github.com>
2024-04-02 16:57:02 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,12 +8,14 @@ import java.util.Scanner;
 /**
  * NotFacebook Main
  *
- * This class contains the main method that allows users to go through the process of signing up, logging in, searching for users, and deleting accounts.
- * It implements the control flow of the program and is how users will interact with the Database and UserProfile classes in Phase 1.
+ * This class contains the main method that allows users to go through the process 
+ * of signing up, logging in, searching for users, and deleting accounts.
+ * It implements the control flow of the program and is how users will 
+ * interact with the Database and UserProfile classes in Phase 1.
  *
  * @author Team 5, 19
  *
- * April 1st, 2024
+ * @version April 1, 2024
  */
 
 public class Main {
