345231403a2fc060a3776263dd730c6f689f77ca
kkita8 <165527193+kkita8@users.noreply.github.com>
2024-04-02 16:56:00 -0400

    Update DatabaseTests.java

--- DatabaseTests.java
+++ DatabaseTests.java
@@ -3,7 +3,8 @@ import static org.junit.Assert.*;
 /**
  * NotFacebook DatabaseTests
  * This program contains tests to verify the functionality of the database methods.
- * It contains 5 tests, checking the ability to recognize existing usernames, correct login info, and addition of new users into the database
+ * It contains 5 tests, checking the ability to recognize existing usernames, 
+ * correct login info, and addition of new users into the database
  * @author Team 5, 19
  *
  * @version April 1, 2024
@@ -14,7 +15,8 @@ public class DatabaseTests {
     Database database = new Database("TestDatabase.txt");
 
     @Test
-    public void writeFileTest() { //THIS TEST WILL CREATE A NEW USER WITH THE INPUTTED INFORMATION. PASSING MEANS DATABASE CLASS CAN SUCESSFULLY ADD USERS TO THE DATABASE
+    public void writeFileTest() { //THIS TEST WILL CREATE A NEW USER WITH THE INPUTTED INFORMATION. 
+        // PASSING MEANS DATABASE CLASS CAN SUCESSFULLY ADD USERS TO THE DATABASE
         database.createFile();
         String username = "testUsername";
         String password = "testPassword";
@@ -23,7 +25,8 @@ public class DatabaseTests {
     }
 
     @Test
-    public void existingUsernameTest() { //THIS TEST WILL ATTEMPT TO DETECT IF THE INPUTTED USERNAME ALREADY EXISTS. PASSING MEANS DATABASE WILL PREVENT SIGNING UP WITH DUPLICATE NAME
+    public void existingUsernameTest() { //THIS TEST WILL ATTEMPT TO DETECT IF THE INPUTTED USERNAME 
+        //ALREADY EXISTS. PASSING MEANS DATABASE WILL PREVENT SIGNING UP WITH DUPLICATE NAME
         database.createFile();
         String username = "testUsername";
         boolean signUpResult = database.readFile1(username);
@@ -31,7 +34,8 @@ public class DatabaseTests {
     }
 
     @Test
-    public void readFileTest() { //THIS TEST WILL VERIFY THAT THE INPUTTED LOGIN MATCHES THE USER INFO IN THE DATABASE. PASSING MEANS LOGIN INFORMATION WAS CORRECT
+    public void readFileTest() { //THIS TEST WILL VERIFY THAT THE INPUTTED LOGIN MATCHES 
+        //THE USER INFO IN THE DATABASE. PASSING MEANS LOGIN INFORMATION WAS CORRECT
         database.createFile();
         String username = "testUsername";
         String password = "testPassword";
@@ -39,7 +43,8 @@ public class DatabaseTests {
         assertTrue(loginResult);
     }
     @Test
-    public void incorrectPassTest() { //THIS TEST WILL ATTEMPT TO LOG IN WITH AN INCORRECT PASSWORD. PASSING MEANS THE DATABASE CLASS SUCCESSFULLY PREVENTS LOGIN
+    public void incorrectPassTest() { //THIS TEST WILL ATTEMPT TO LOG IN WITH AN 
+        //INCORRECT PASSWORD. PASSING MEANS THE DATABASE CLASS SUCCESSFULLY PREVENTS LOGIN
         database.createFile();
         String username = "testUsername";
         String password = "yabadabadoo";
@@ -48,7 +53,8 @@ public class DatabaseTests {
     }
 
     @Test
-    public void incorrectUsernameTest() { //THIS TEST WILL ATTEMPT TO LOG IN WITH A USERNAME THAT DOESN'T EXIST. PASSING MEANS THE DATABASE CLASS SUCCESSFULLY PREVENTS LOGIN
+    public void incorrectUsernameTest() { //THIS TEST WILL ATTEMPT TO LOG IN WITH A USERNAME 
+        //THAT DOESN'T EXIST. PASSING MEANS THE DATABASE CLASS SUCCESSFULLY PREVENTS LOGIN
         database.createFile();
         String username = "yabadabadoo";
         String password = "testPassword";
