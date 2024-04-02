82a99cf217e475923f9f5c466cfdbb1dd3b9f122
RonakHegde17 <139830762+RonakHegde17@users.noreply.github.com>
2024-04-01 22:49:12 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,6 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         boolean loggedIn = false;
-        boolean exit = false;
         String email;
         String username = "";
         String password = "";
@@ -19,13 +18,10 @@ public class Main {
         boolean validPassword;
         int mainMenuChoice;
         int deleteChoice;
+        String searchUser;
 
         Database database = new Database("database.txt");
         database.createFile();
-
-
-
-
 
         System.out.println("Welcome to NotFacebook!");
 
@@ -37,7 +33,7 @@ public class Main {
             logChoice = scanner.nextInt();
             scanner.nextLine();
 
-            switch (logChoice) {
+            switch (logChoice) { //CHOOSING TO SIGN UP
                 case 1:
                     while (!validUsername) {
                         System.out.println("Enter a username:");
@@ -50,15 +46,13 @@ public class Main {
                             database.writeFile1(username, password);
                             UserProfile user = new UserProfile(username, password);
                             database.addUser(user);
-
-
                         }
                     }
 
                     break;
 
                 case 2:
-                    while (!validUsername) {
+                    while (!validUsername) { //CHOOSING TO LOG IN
                         System.out.println("Enter your username:");
                         username = scanner.nextLine();
                         if (database.readFile2(username)) {
@@ -86,26 +80,40 @@ public class Main {
 
         }
 
-        while (loggedIn) {
+        while (loggedIn) { //ACCOUNT OPTIONS AFTER SUCCESSFUL LOGIN
             System.out.println("1: View Account");
-            System.out.println("2: View Friends");
-            System.out.println("3: Logout");
-            System.out.println("4: Delete Account");
+            System.out.println("2: Search Users");
+            System.out.println("3: View Friends");
+            System.out.println("4: Logout");
+            System.out.println("5: Delete Account");
             mainMenuChoice = scanner.nextInt();
             scanner.nextLine();
             switch (mainMenuChoice) {
                 case 1:
                     System.out.println("Username: " + username);
                     System.out.println("Password: " + password);
+                    System.out.println();
                     break;
                 case 2:
-                    System.out.println("No Friends Added");
+                    System.out.println("Enter a Username:");
+                    searchUser = scanner.nextLine();
+                    if (database.searchFile(searchUser)) {
+                        System.out.println("Username: " + searchUser);
+                        System.out.println();
+                    } else {
+                        System.out.println("User not found");
+                        System.out.println();
+                    }
                     break;
-
                 case 3:
+                    System.out.println("No Friends Added");
+                    System.out.println();
+                    break;
+                case 4:
                     System.out.println("Logging Out...");
                     loggedIn = false;
-                case 4:
+                    break;
+                case 5:
                     System.out.println("Are you sure you want to delete your account?");
                     System.out.println("1: No");
                     System.out.println("2: Yes");
